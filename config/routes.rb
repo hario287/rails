8396af0be0021fa39589ce78bbc3ad172af4aff6
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-11 09:18:53 +0000

    [Finish] raisl-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# HTTPメソッド 'URL' => 'コントローラ#アクション'
+
+# URL「top」にアクセスしたときに、
+# homesコントローラのtopアクションが呼び出されるよう設定
+get 'top' => 'homes#top'
+
+# homes_controllerのtopアクションに
+# URL /home/topsを対応させたい時
+# get 'home/tops' => 'homes#top'
+
+
 end
